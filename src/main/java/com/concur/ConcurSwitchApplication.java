ad18773b1d71b6f6f8da3f41d6a1897f770f82c9
JVictorio <jvictorio@aimconsulting.com>
2016-01-07 13:50:11 -0800

    Initial commit to master

---
+++ src/main/java/com/concur/ConcurSwitchApplication.java
@@ -0,0 +1,13 @@
+package com.concur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurSwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurSwitchApplication.class, args);
+		System.out.println("Hello Jun");
+	}
+}
